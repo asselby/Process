59f2caed36974ce44c54f26a93fc8825361fceb9
Асель Баймуханбетова <aselya119933@mail.ru>
2018-03-14 16:26:26 +0600

    first commit

---
+++ Process/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ProcessFirstHW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TextLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(38, 75);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(728, 404);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(285, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Показать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.show_button);
+            // 
+            // TextLabel
+            // 
+            this.TextLabel.AutoSize = true;
+            this.TextLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TextLabel.Location = new System.Drawing.Point(33, 25);
+            this.TextLabel.Name = "TextLabel";
+            this.TextLabel.Size = new System.Drawing.Size(237, 25);
+            this.TextLabel.TabIndex = 2;
+            this.TextLabel.Text = "Свойства процесса: ";
+            this.TextLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 491);
+            this.Controls.Add(this.TextLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label TextLabel;
+    }
+}
+
---
+++ Process/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ProcessFirstHW
+{
+    public partial class Form1 : Form
+    {
+               public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void show_button(object sender, EventArgs e)
+        {
+            var allProcess = from p in Process.GetProcesses()
+                             orderby p.Id
+                             select new
+                             {
+                                 p.Id,
+                                 p.MachineName,
+                                 p.ProcessName,
+                                 p.MainWindowTitle,
+                                 p.Threads
+                             };
+                            
+
+            foreach (var item in allProcess)
+            {
+              listView1.Items.Add(item.ToString());
+
+            }
+       
+        }
+
+
+    }
+}
